e1751ef5d0614fecad2bff93298528e263fc95ec
Tamara Cook <Tamara.Cook@icloud.com>
2015-06-01 22:55:41 +0200

    added config for assets.root and assets.index

--- index.js
+++ index.js
@@ -4,22 +4,21 @@ var koa=require('koa');
 var send=require('koa-send');
 var helmet=require('koa-helmet');
 var api=require('./api');
-//var mount=require('koa-mount');
 var mongoose=require('mongoose');
 var config=require('config');
 var server=config.get('server');
 var db=config.get('db');
+var assets=config.get('assets');
 var app=koa();
 require('koa-qs')(app);
 require('koa-onerror')(app);
 app.use(require('koa-morgan').middleware('dev'))
 .use(helmet.defaults())
-//.use(mount('/api',require('./api')))
 .use(api.routes())
 .use(api.allowedMethods())
-.use(require('koa-static')('../client/dist'))
+.use(require('koa-static')(assets.root))
 .use(function *(){
-  yield send(this, 'index.html',{root: '../client/dist'});
+  yield send(this, assets.index,{root: assets.root});
 })
 .listen(server.port,function(){
   console.log(`listening on ${server.host}:${server.port}`);
